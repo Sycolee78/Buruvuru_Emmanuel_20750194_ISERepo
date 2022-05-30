79f4fc3e0cf8e25dd2e3fc24be587fd06863291c
Emmanuel Buruvuru <emmanuelburuvuru@Emmanuels-MacBook-Pro.local>
2022-05-30 20:13:04 +0400

    Modified production code

--- code/Category1.py
+++ code/Category1.py
@@ -4,134 +4,84 @@
 #Refrences: 
 #
 
-import string
 
+def LowertoUpper(string: str): 
+    
+    a = string.upper()
+    
+    return a
 
-class category: 
+def UppertoLower(string: str): 
 
-    def UppertoLower(string: str) : 
+    b = string.lower()
+    
+    return b
+
+def NumericsinStrings(string: str):
+
+    res = True if next((chr for chr in string if chr.isdigit()), None) else False 
+
+    if res == True: 
+        return "True"
+
+    else: 
+        return "False"
+    
+
+def IsStringValid_number(string: str):
+
+    i = 0
+    j = len(string) - 1
+
+    if (i == j and not(string[i] >= '0' and 
+                    string[i] <= '9')):
+        return False
+
+    # If the 1st char is not '+', '-', '.' or digit
+    if (string[i] != '.' and string[i] != '+' and 
+        string[i] != '-' and not(string[i] >= '0' and 
+        string[i] <= '9')):
+        return print("String is not a valid number..")
+
+    DotorE = False
+
+    for i in range(j + 1):
         
-        a = string.upper() s
-        
-        return a
+        if (string[i] != 'e' and string[i] != '.' and 
+            string[i] != '+' and string[i] != '-' and not
+        (string[i] >= '0' and string[i] <= '9')):
+            return print("String is not a valid number..")
 
-    def LowertoUpper(string: str): 
-
-        b = string.lower()
-        
-        return b
-
-    def NumericsinStrings(string: str):
-
-        res = True if next((chr for chr in string if chr.isdigit()), None) else False
-
-        if res == True: 
-            print("The string contains a numeric value :) ")
-
-        else: 
-            print("The string does not contain a numeric value :( ")
-        
-
-    def IsStringValid(string: str): 
-
-        numeric = ["0","1","2","3","4","5","6","7","8","9"]
-
-        for i in numeric:
-            for j in string:  
-                if i == j: 
-                    print("The string is numneric value..")
-
-                else:
-                    
-                    print("The string is  not  a numneric value..")
-
-        return
-
-    def valid_number(string: str):
-
-        i = 0
-        j = len(string) - 1
-    
-        while i<len(string) and string[i] == ' ':
-            i += 1
-        while j >= 0 and string[j] == ' ':
-            j -= 1
-    
-        if i > j:
-            return False
-
-        if (i == j and not(string[i] >= '0' and 
-                        string[i] <= '9')):
-            return False
-    
-        # If the 1st char is not '+', '-', '.' or digit
-        if (string[i] != '.' and string[i] != '+' and 
-            string[i] != '-' and not(string[i] >= '0' and 
-            string[i] <= '9')):
-            return False
-    
-        flagDotOrE = False
-    
-        for i in range(j + 1):
+        if string[i] == '.':
             
-            if (string[i] != 'e' and string[i] != '.' and 
-                string[i] != '+' and string[i] != '-' and not
-            (string[i] >= '0' and string[i] <= '9')):
+            if DotorE or i + 1 > len(string) or (not(string[i + 1] >= '0' and string[i + 1] <= '9')):
+                return print("String is a valid number..")
+        elif string[i] == 'e':
+            
+            DotorE = True
+
+            if (not(string[i - 1] >= '0' and 
+                    string[i - 1] <= '9')):
                 return False
-    
-            if string[i] == '.':
+
+            if i + 1 > len(string):
+                return False
+
+            if (string[i + 1] != '+' and string[i + 1] != '-' and 
+            (string[i + 1] >= '0' and string[i] <= '9')):
+                return False
                 
-                if flagDotOrE:
-                    return False
-                if i + 1 > len(string):
-                    return False
-                if (not(string[i + 1] >= '0' and 
-                        string[i + 1] <= '9')):
-                    return False
-            elif string[i] == 'e':
-                
-                flagDotOrE = True
+    return print("String is a valid number..")
+
+
+def remove_numeric(string: str): 
+
+    res = ''.join([i for i in string if not i.isdigit()])
     
-                if (not(string[i - 1] >= '0' and 
-                        string[i - 1] <= '9')):
-                    return False
 
-                if i + 1 > len(string):
-                    return False
- 
-                if (string[i + 1] != '+' and string[i + 1] != '-' and 
-                (string[i + 1] >= '0' and string[i] <= '9')):
-                    return False
-                    
-        return True
+    return print("final string:", res.upper())
 
 
-    def remove_numeric(sting: str): 
-  
-        res = ''.join([i for i in string if not i.isdigit()])
-        
-        print("final string : ", res)
-
-        a = str(input("Do you want to convert the string to (U)pper or (L)ower case: ")).upper() 
-
-        if a == 'U':
-
-            conversion = res.upper() 
-
-        elif a == 'L': 
-
-            conversion = res.lower() 
-
-        else: 
-            print("Enter U to convert to Upper\n Enter L to conver to Lower: ")
-
-        return conversion 
-
-if __name__ == '__main__': 
-    string = input("Enter a String to convert: ")       
-    a = category.valid_number(string)
-
-    print("The string converted is -> ", a)  
 
 
 
--- code/Category2.py
+++ code/Category2.py
@@ -1,21 +1,36 @@
 #
 #Author : Emmanuel Buruvuru
-#Catergorey2.py : for the version control of the assignment 
+#Catergorey2.py : Module for time conversion of assignment two.  
 #Refrences: 
 #
 
 
 class category2: 
 
-    def HrsToMinMinToSec(time: int):   #mutiplying time 
+    def HrsToMinMinToSec(time: int):   #Sub-module to mutiplying time to given conversion.
 
-        conversion = time * 60
+        if time < 0: 
+            conversion = "input a positive int" 
+
+        elif time == 0: 
+            conversion = "Number should not be zero"
+
+        else: 
+            conversion = time * 60
 
         return conversion 
 
-    def MinToHrsSecToMin(time: int):  #divinding time 
+    def MinToHrsSecToMin(time: int):  #Sub-module that deals with divinding time  to a given conversion and returns a float
 
-        conversion = time / 60
+        if time < 0: 
+            conversion = "input a positive int" 
+
+        elif time == 0: 
+            conversion = "Number should not be zero"
+            
+        else: 
+
+            conversion = time / 60
 
         return conversion
 
@@ -29,13 +44,13 @@ def saveData(filename, Hours, Minutes, Seconds):
 
     
     try:
-        with open(filename, mode = "w") as writer: # Raises OSError
+        with open(filename, mode = "w") as writer: 
             writer.write("Hours to minutes: " + str(d) + "\n")
             writer.write("Minutes to Hours: " + str(a) + "\n")
             writer.write("Seconds to minutes: " + str(c) + "\n")
             writer.write("Minutes to seconds: " + str(b) + "\n")
                     
-    except OSError:
+    except OSError:       # Raises OSError
         success = False
         
     return success
